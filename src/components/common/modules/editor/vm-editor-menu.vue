598890a0bb6581f76d77617976c7501d04db746b
You <you85630@foxmail.com>
2019-08-02 18:43:34 +0800

    init

--- src/components/common/modules/editor/vm-editor-menu.vue
+++ src/components/common/modules/editor/vm-editor-menu.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="vm-editor-menu">
     <VmEditorButton icon="paragraph" @click.native="execCommand('formatBlock', '<p>')"></VmEditorButton>
+    <!-- 字体 -->
     <VmEditorButton icon="heading">
       <VmEditorDropdown>
         <ul class="vm-editor-ul">
@@ -32,6 +33,7 @@
         </ul>
       </VmEditorDropdown>
     </VmEditorButton>
+    <!-- 字体 -->
     <VmEditorButton icon="font-size">
       <VmEditorDropdown>
         <ul class="vm-editor-ul">
@@ -59,50 +61,60 @@
         </ul>
       </VmEditorDropdown>
     </VmEditorButton>
+    <!-- 粗体 -->
     <VmEditorButton icon="bold" @click.native="execCommand('bold')"></VmEditorButton>
+    <!-- 斜体 -->
     <VmEditorButton icon="italic" @click.native="execCommand('italic')"></VmEditorButton>
+    <!-- 下划线 -->
     <VmEditorButton icon="underline" @click.native="execCommand('underline')"></VmEditorButton>
+    <!-- 中划线 -->
     <VmEditorButton icon="strikethrough" @click.native="execCommand('strikeThrough')"></VmEditorButton>
+    <!-- 字体颜色 -->
     <VmEditorButton icon="font-color">
       <VmEditorDropdown>
         <VmEditorFontcolor></VmEditorFontcolor>
       </VmEditorDropdown>
     </VmEditorButton>
-
+    <!-- 分割线 -->
     <span class="line"></span>
-
+    <!-- 有序列表 -->
     <VmEditorButton icon="ol" @click.native="execCommand('insertOrderedList')"></VmEditorButton>
+    <!-- 无需列表 -->
     <VmEditorButton icon="ul" @click.native="execCommand('insertUnorderedList')"></VmEditorButton>
+    <!-- 引用1 -->
     <VmEditorButton icon="quote"  @click.native="execCommand('formatBlock', '<blockquote>')"></VmEditorButton>
+    <!-- 引用2 -->
     <VmEditorButton icon="code" @click.native="execCommand('formatBlock', '<pre>')"></VmEditorButton>
     <!-- <VmEditorButton icon="fa fa-table"></VmEditorButton> -->
-
+    <!-- 分割线 -->
     <span class="line"></span>
-
+    <!-- 上传图片 -->
     <VmEditorButton icon="image">
       <VmEditorDropdown>
         <VmEditorAddimage></VmEditorAddimage>
       </VmEditorDropdown>
     </VmEditorButton>
-
+    <!-- 上传链接 -->
     <VmEditorButton icon="link">
       <VmEditorDropdown>
         <VmEditorAddlink></VmEditorAddlink>
       </VmEditorDropdown>
     </VmEditorButton>
+    <!-- 内容分割线 -->
     <VmEditorButton icon="line" @click.native="execCommand('insertHorizontalRule')"></VmEditorButton>
-
+    <!-- 分割线 -->
     <span class="line"></span>
-
+    <!-- 对齐方式 -->
     <VmEditorButton icon="align-center" @click.native="execCommand('justifyCenter')"></VmEditorButton>
     <VmEditorButton icon="align-left" @click.native="execCommand('justifyLeft')"></VmEditorButton>
     <VmEditorButton icon="align-right" @click.native="execCommand('justifyRight')"></VmEditorButton>
     <VmEditorButton icon="justify" @click.native="execCommand('justifyFull')"></VmEditorButton>
-
+    <!-- 分割线 -->
     <span class="line"></span>
-
+    <!-- 清空 -->
     <VmEditorButton icon="eraser" @click.native="execCommand('removeFormat')"></VmEditorButton>
     <VmEditorButton icon="trash" @click.native="execCommand('delete')"></VmEditorButton>
+    <!-- 其他 -->
     <slot></slot>
   </div>
 </template>
