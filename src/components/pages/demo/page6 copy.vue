99520ae6af4fae63d256c6582d55dc31bddf1bbb
You <you85630@foxmail.com>
2019-08-24 18:16:28 +0800

    init

---
+++ src/components/pages/demo/page6 copy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <editor-wrap></editor-wrap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page6',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.box{
+  width: 100%;
+  height: 400px;
+}
+</style>
--- src/components/pages/demo/page6.vue
+++ src/components/pages/demo/page6.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box">
-    <editor-wrap></editor-wrap>
+    <div id="editor1" class="box"></div>
+    <div id="editor2" class="box"></div>
   </div>
 </template>
 
@@ -10,12 +11,33 @@ export default {
   data () {
     return {
     }
+  },
+  created () {},
+  mounted () {
+    let Editor = require('wangeditor')
+    let editor1 = new Editor('#editor1')
+    editor1.customConfig.menus = [
+      'fontSize', // 字号
+      'bold', // 粗体
+      'italic', // 斜体
+      'underline', // 下划线
+      'strikeThrough', // 删除线
+      'list', // 列表
+      'justify', // 对齐方式
+      'quote', // 引用
+      'link', // 插入链接
+      'image' // 插入图片
+    ]
+    editor1.create()
+
+    let editor2 = new Editor('#editor2')
+    editor2.create()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.box{
+.box {
   width: 100%;
   height: 400px;
 }
