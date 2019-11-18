f149ab237c4eddca4e29b29c69f992b5d93f633c
You <youjinjin@You.local>
2019-11-18 20:50:22 +0800

    init

--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrap">
     <h1>构成：vue2.6.10 + view-design4.0.0 + vuex3.0.1</h1>
-    <h2>自定义组件：</h2>
+    <h2>1.自定义组件：</h2>
     <h2>
       <ul>
         <li>Tab分页切换组件：联动右侧导航</li>
@@ -10,6 +10,7 @@
         <li>城市联动选择器：iview/Cascader 级联选择</li>
       </ul>
     </h2>
+    <h2>2.多层嵌套导航</h2>
   </div>
 </template>
 
@@ -22,5 +23,14 @@ export default {
 <style lang="scss" scoped>
 .wrap {
   font-size: 14px;
+  h2 {
+    margin-top: 20px;
+  }
+  ul {
+    margin-left: 20px;
+    li {
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
