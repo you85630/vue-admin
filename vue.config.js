5c858d5868160347b19916ca42f30f167d87d5f6
You <youjinjin@You.local>
2019-11-18 21:16:02 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
+  publicPath: '/',
   productionSourceMap: false,
   runtimeCompiler: true,
   assetsDir: 'assets',
